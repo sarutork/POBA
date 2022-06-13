27fb976b9e179588d5fad2b8359d63cd98d67dcd
ekmn <ekmn@dnamaker.com>
2022-06-13 22:07:45 +0700

    Fix quiry studentsummary

--- src/main/java/com/obu/tech/poba/students/StudentsSummaryRepository.java
+++ src/main/java/com/obu/tech/poba/students/StudentsSummaryRepository.java
@@ -4,10 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface StudentsSummaryRepository extends JpaRepository<StudentsSummary, Long>, JpaSpecificationExecutor<StudentsSummary> {
-    @Query("SELECT name,surname,students_year as year,COUNT(*) AS total,ANY_VALUE(CONCAT(name,' ',surname)) AS fullname FROM students WHERE students_year = :year GROUP BY name,surname,year")
+@Repository
+public interface StudentsSummaryRepository extends JpaRepository<Students, Long>, JpaSpecificationExecutor<Students> {
+    @Query("FROM Students WHERE studentsYear = :year")
     List<StudentsSummary> findRoles(@Param("year") String year);
 }
