a1b1a5a0db952bf5903c685e4a5eb35a12afab86
titanooz <shadow.neverdie@gmail.com>
2022-07-17 23:35:56 +0700

    add permission

--- src/main/java/com/obu/tech/poba/consultant_info/thesis/ConsultantThesisController.java
+++ src/main/java/com/obu/tech/poba/consultant_info/thesis/ConsultantThesisController.java
@@ -1,9 +1,11 @@
 package com.obu.tech.poba.consultant_info.thesis;
 
+import com.obu.tech.poba.authenticate.MemberAccess;
 import com.obu.tech.poba.personnel_info.profile.Profile;
 import com.obu.tech.poba.personnel_info.profile.ProfileService;
 import com.obu.tech.poba.students.Students;
 import com.obu.tech.poba.students.StudentsService;
+import com.obu.tech.poba.utils.MemberAccessUtils;
 import com.obu.tech.poba.utils.NameConverterUtils;
 import com.obu.tech.poba.utils.YearGeneratorUtils;
 import com.obu.tech.poba.utils.exceptions.InvalidInputException;
@@ -19,13 +21,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.security.RolesAllowed;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
 
+import static com.obu.tech.poba.utils.role.Roles.*;
 import static com.obu.tech.poba.utils.upload.UploadService.UPLOAD_GROUP_THESIS;
 
 @Slf4j
 @Controller
+@RolesAllowed(ROLE_CONSULTANT_THESIS_ACCESS)
 @RequestMapping("/consultant/thesis")
 public class ConsultantThesisController {
     static final String FRAGMENT_CONSULTANT_THESIS = "consultant-info/consultant-thesis :: consultant-thesis";
@@ -58,29 +65,43 @@ public class ConsultantThesisController {
     @Value("${poba.upload.thesis}")
     private String UPLOAD_THESIS_PATH;
 
-    @GetMapping
-    public ModelAndView showListView() {return new ModelAndView(FRAGMENT_CONSULTANT_THESIS);}
+    @Autowired
+    private MemberAccessUtils memberAccessUtils;
 
+    @GetMapping
+    public ModelAndView showListView(HttpServletRequest request) {
+        ModelAndView view = new ModelAndView(FRAGMENT_CONSULTANT_THESIS);
+        MemberAccess member = memberAccessUtils.getMemberAccess(request);
+        view.addObject("user",member.getMember());
+        view.addObject("roles",member.getRoles());
+        return view;
+    }
+
+    @RolesAllowed(ROLE_CONSULTANT_THESIS_SEARCH)
     @GetMapping("/search")
     public ResponseEntity<List<ConsultantThesis>> search(@ModelAttribute ConsultantThesis consultantThesis) {
         return ResponseEntity.ok().body(consultantThesisService.findBySearchCriteria(consultantThesis));
     }
 
+    @RolesAllowed(ROLE_CONSULTANT_THESIS_ADD)
     @GetMapping("/add")
-    public ModelAndView add() {
+    public ModelAndView add(HttpServletRequest request) {
        ConsultantThesis thesis = new ConsultantThesis();
        Journal journal = new Journal();
        AcademicConference acdConf = new AcademicConference();
 
-       return formAdd(thesis,journal,acdConf);
+       return formAdd(thesis,journal,acdConf,request);
     }
+    @RolesAllowed({ROLE_CONSULTANT_THESIS_ADD,ROLE_CONSULTANT_THESIS_EDIT})
     @RequestMapping(path = "/save", method = { RequestMethod.POST, RequestMethod.PUT , RequestMethod.PATCH}, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public ModelAndView save(@ModelAttribute("thesis") @Valid ConsultantThesis thesis, BindingResult bindingResultThesis,
                              @ModelAttribute("journal") @Valid Journal journal, BindingResult bindingResultJournal,
-                             @ModelAttribute("acdConf") @Valid AcademicConference acdConf, BindingResult bindingResultAcdConf) {
+                             @ModelAttribute("acdConf") @Valid AcademicConference acdConf,
+                             BindingResult bindingResultAcdConf,
+                             HttpServletRequest request) {
         if (bindingResultThesis.hasErrors() || bindingResultJournal.hasErrors() || bindingResultAcdConf.hasErrors()) {
             throw new InvalidInputException(
-                    formAdd(thesis,journal,acdConf), bindingResultThesis, bindingResultJournal, bindingResultAcdConf);
+                    formAdd(thesis,journal,acdConf,request), bindingResultThesis, bindingResultJournal, bindingResultAcdConf);
         }
 
         try {
@@ -128,18 +149,25 @@ public class ConsultantThesisController {
             thesisRes.setStudentsLevel(thesis.getStudentsLevel());
             thesisRes.setCourse(thesis.getCourse());
 
-            return viewSuccess(thesisRes, journalRes, acdConfRes);
+            return viewSuccess(thesisRes, journalRes, acdConfRes,request);
 
         }catch (Exception e){
             e.printStackTrace();
             log.error("{}: {}", e.getClass().getSimpleName(), e.getMessage());
-            return new ModelAndView(FRAGMENT_CONSULTANT_THESIS_FORM).addObject("responseMessage", "ไม่สำเร็จ");
+            ModelAndView view = new ModelAndView(FRAGMENT_CONSULTANT_THESIS);
+            MemberAccess member = memberAccessUtils.getMemberAccess(request);
+            view.addObject("user",member.getMember());
+            view.addObject("roles",member.getRoles());
+            view.addObject("responseMessage", "ไม่สำเร็จ");
+            return view;
+
 
         }
     }
 
+    @RolesAllowed({ROLE_CONSULTANT_THESIS_SEARCH,ROLE_CONSULTANT_THESIS_EDIT})
     @GetMapping(value = "/{id}")
-    public ModelAndView showInfo(@PathVariable String id){
+    public ModelAndView showInfo(@PathVariable String id,HttpServletRequest request){
         ConsultantThesis thesis = consultantThesisService.findById(id);
         if(thesis.getPersNo() !=null) {
             Profile profile = profileService.findByPersNo(thesis.getPersNo());
@@ -163,24 +191,33 @@ public class ConsultantThesisController {
         List<Upload> uploads = uploadService.getByGroupAndReference(UPLOAD_GROUP_THESIS, academicConference.getConferenceId());
         academicConference.setUploads(uploads);
 
-        return view(thesis, journal, academicConference);
+        return view(thesis, journal, academicConference,request);
     }
 
-    private ModelAndView formAdd(ConsultantThesis thesis, Journal journal, AcademicConference acdConf) {
-        return form(thesis,journal,acdConf).addObject("viewName", "เพิ่มข้อมูล");
+    private ModelAndView formAdd(ConsultantThesis thesis, Journal journal,
+                                 AcademicConference acdConf,HttpServletRequest request) {
+        return form(thesis,journal,acdConf,request).addObject("viewName", "เพิ่มข้อมูล");
     }
 
-    private ModelAndView form(ConsultantThesis thesis, Journal journal, AcademicConference acdConf) {
+    private ModelAndView form(ConsultantThesis thesis, Journal journal,
+                              AcademicConference acdConf,HttpServletRequest request) {
         List<Integer> years = yearGeneratorUtils.genYears();
-        return new ModelAndView(FRAGMENT_CONSULTANT_THESIS_FORM)
-                .addObject("thesis", thesis)
-                .addObject("journal",journal)
-                .addObject("acdConf",acdConf)
-                .addObject("years", years);
+        ModelAndView view = new ModelAndView(FRAGMENT_CONSULTANT_THESIS_FORM);
+        MemberAccess member = memberAccessUtils.getMemberAccess(request);
+        view.addObject("user",member.getMember());
+        view.addObject("roles",member.getRoles());
+        view.addObject("thesis", thesis);
+        view.addObject("journal",journal);
+        view.addObject("acdConf",acdConf);
+        view.addObject("years", years);
+        return view;
     }
 
-    private ModelAndView viewSuccess(ConsultantThesis thesis, Journal journal, AcademicConference acdConf) {
-        return view(thesis,journal,acdConf)
+    private ModelAndView viewSuccess(ConsultantThesis thesis,
+                                     Journal journal,
+                                     AcademicConference acdConf,
+                                     HttpServletRequest request) {
+        return view(thesis,journal,acdConf,request)
                 .addObject("viewName", "ดูข้อมูล")
                 .addObject("responseMessage", "บันทึกสำเร็จ")
                 .addObject("success", true) // success green, else red
@@ -188,13 +225,21 @@ public class ConsultantThesisController {
                 ;
     }
 
-    private ModelAndView view(ConsultantThesis thesis, Journal journal, AcademicConference acdConf) {
+    private ModelAndView view(ConsultantThesis thesis,
+                              Journal journal,
+                              AcademicConference acdConf,
+                              HttpServletRequest request) {
         List<Integer> years = yearGeneratorUtils.genYears();
-        return new ModelAndView(FRAGMENT_CONSULTANT_THESIS_FORM).addObject("viewName", "ดูข้อมูล")
-                .addObject("thesis", thesis)
-                .addObject("journal",journal)
-                .addObject("acdConf",acdConf)
-                .addObject("years", years);
+        ModelAndView view = new ModelAndView(FRAGMENT_CONSULTANT_THESIS_FORM);
+        MemberAccess member = memberAccessUtils.getMemberAccess(request);
+        view.addObject("user",member.getMember());
+        view.addObject("roles",member.getRoles());
+        view.addObject("viewName", "ดูข้อมูล");
+        view.addObject("thesis", thesis);
+        view.addObject("journal",journal);
+        view.addObject("acdConf",acdConf);
+        view.addObject("years", years);
+        return view;
     }
 
 
